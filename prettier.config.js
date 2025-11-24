e3262c98627cd777fbdf8b0d4fb93b6c8482418c
Jonas <jonasmessias30@gmail.com>
2025-11-24 08:53:14 -0300

    chore: initial project setup with Next.js 15 and TypeScript

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSameLine: true,
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'auto',
+
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindAttributes: ['className'],
+}
